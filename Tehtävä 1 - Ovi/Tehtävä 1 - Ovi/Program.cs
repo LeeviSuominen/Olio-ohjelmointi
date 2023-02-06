8a37f4a619fbd459111dd3ca13707b3612f03af2
LeeviSuominen <115700342+LeeviSuominen@users.noreply.github.com>
2023-02-06 13:59:43 +0200

    Task 1 - Door

---
+++ Tehtävä 1 - Ovi/Tehtävä 1 - Ovi/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Ovi
+{
+
+    class Program
+    {
+        enum DoorState
+        {
+            Locked,
+            Closed,
+            Open,
+            Unlocked
+        }
+
+        static DoorState doorNow = DoorState.Locked;
+        
+        static void Main(string[] args)
+        {
+            AskPlayer();
+        }
+
+        static void AskPlayer()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Door is {doorNow}. What do you want to do? (Unlock, Close, Open, Lock)");
+                string? input = Console.ReadLine();
+
+                switch (doorNow)
+                {
+                    case DoorState.Locked:
+                        if(input == "Unlock")
+                        {
+                            doorNow= DoorState.Unlocked;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("The door stays locked. Enter the right command.");
+                        }
+                        break;
+
+                        case DoorState.Unlocked:
+                        if (input == "Lock")
+                        {
+                            doorNow= DoorState.Locked;
+                        }
+
+                        else if (input == "Open")
+                        {
+                            doorNow= DoorState.Open;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("The door is unlocked. Enter 'Open' or 'Lock'. ");
+                        }
+                        break;
+
+                        case DoorState.Open:
+                        if (input == "Close")
+                        {
+                            doorNow= DoorState.Closed;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("The door is open. Enter 'Close' to close it");
+                        }
+                        break;
+
+                        case DoorState.Closed:
+                        if (input == "Lock")
+                        {
+                            doorNow = DoorState.Locked;
+                        }
+
+                        else if (input == "Open")
+                        {
+                            doorNow = DoorState.Open;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("The door is closed. Enter 'Open', or 'Lock'");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
+
